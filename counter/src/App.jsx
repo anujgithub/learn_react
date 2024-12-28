ff659fc6e94578d8dc0a648b842ce6d3ba66aae9
Anuj Gupta <anuj.gupta@3pillar.corp>
2024-12-28 14:50:50 +0530

    counter project

---
+++ counter/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addCounter = () => {
+    if(count === 10) return
+    setCount((count) => count + 1)
+  }
+
+  const subtractCounter = () => {
+    if(count === 0) return
+    setCount((count) => count - 1)
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <strong>Count is  {count}</strong>
+        <br />
+        <button onClick={addCounter}>
+          Add Counter
+        </button>
+        <br />
+        <button onClick={subtractCounter}>
+          Subtract Counter
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
